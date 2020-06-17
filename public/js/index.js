fba8844afeedea7f365fa0274ed0da9508ef67bd
CodyHoover <codydevloop@gmail.com>
2020-06-17 06:40:25 -0700

    deleted out all unused code in index.js for frontend

--- public/js/index.js
+++ public/js/index.js
@@ -14,8 +14,7 @@ $(document).ready(function () {
   let $buttonTypeAndData = $(".buttontype");
 
 
-  // object to hold current search results, for running functions on
-  let genreTopTenThouObj = {};
+
   // The API object contains methods for each kind of request we'll make
   var API = {
 
@@ -30,13 +29,7 @@ $(document).ready(function () {
       $.ajax("/api/fullmovie/" +movie ,{
         type: "GET"
       }).then(function (response) {
-        // console.log(response);
-        //response.homepage
-        //response.id
-        //response.title
-        //response.poster_path$
-        //response.vote_average
-        //respnse.overview
+
         let data = {
           movieId: response.id,
           poster: `http://image.tmdb.org/t/p/w154${response.posterPath}`,
@@ -81,28 +74,7 @@ $(document).ready(function () {
     // saveLikes: function ()
 
     // Start Danyal expamples
-    saveExample: function (example) {
-      return $.ajax({
-        headers: {
-          "Content-Type": "application/json"
-        },
-        type: "POST",
-        url: "api/examples",
-        data: JSON.stringify(example)
-      });
-    },
-    getExamples: function () {
-      return $.ajax({
-        url: "api/examples",
-        type: "GET"
-      });
-    },
-    deleteExample: function (id) {
-      return $.ajax({
-        url: "api/examples/" + id,
-        type: "DELETE"
-      });
-    }
+    // 
   };
   //##################################
   //############### Project #2 class defined
@@ -140,78 +112,7 @@ $(document).ready(function () {
     API.GETmovies(userSearchTerm);
   }
 
-  // refreshExamples gets new examples from the db and repopulates the list
-  // let refreshExamples = function () {
-  //   API.getExamples().then(function (data) {
-  //     var $examples = data.map(function (example) {
-  //       var $a = $("<a>")
-  //         .text(example.text)
-  //         .attr("href", "/example/" + example.id);
-
-  //       var $li = $("<li>")
-  //         .attr({
-  //           class: "list-group-item",
-  //           "data-id": example.id
-  //         })
-  //         .append($a);
-
-  //       var $button = $("<button>")
-  //         .addClass("btn btn-danger float-right delete")
-  //         .text("ｘ");
-
-  //       $li.append($button);
-
-  //       return $li;
-  //     });
-
-  //     $exampleList.empty();
-  //     $exampleList.append($examples);
-  //   });
-  // };
-
-  // Instructor Code
-  // handleFormSubmit is called whenever we submit a new example
-  // Save the new example to the db and refresh the list
-
-  // let handleFormSubmit = function(event) {
-  //   event.preventDefault();
-
-  //   var example = {
-  //     text: $exampleText.val().trim(),
-  //     description: $exampleDescription.val().trim()
-  //   };
-
-  //   if (!(example.text && example.description)) {
-  //     alert("You must enter an example text and description!");
-  //     return;
-  //   }
-
-  //   API.saveExample(example).then(function() {
-  //     refreshExamples();
-  //   });
-
-  //   $exampleText.val("");
-  //   $exampleDescription.val("");
-  // };
-
-  // handleDeleteBtnClick is called when an example's delete button is clicked
-  // Remove the example from the db and refresh the list
-  let handleDeleteBtnClick = function () {
-    var idToDelete = $(this)
-      .parent()
-      .attr("data-id");
-
-    API.deleteExample(idToDelete).then(function () {
-      refreshExamples();
-    });
-  };
-
-
-  // Add event listeners to the submit and delete buttons
-  // Instructor Code
-  // $submitBtn.on("click", handleFormSubmit);  
-  //$exampleList.on("click", ".delete", handleDeleteBtnClick);
-
+  
 
   // Group Project generated
   $findMovie.on("click", sugarSearch);
